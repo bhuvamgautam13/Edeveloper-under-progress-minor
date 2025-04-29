f263afd477cd03badd0f905a18a9489450265980
DhruvMaheshwarihere <Dhruv1034.becse24@chitkara.edu.in>
2025-04-29 14:02:07 +0530

    added the javascript of blog page

--- script.js
+++ script.js
@@ -85,23 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Tic Tac Toe Game Logic
-    const gameBoard = document.getElementById('game-board');
-    const resetButton = document.getElementById('resetButton');
-    let board = ['', '', '', '', '', '', '', '', ''];
-    let currentPlayer = 'X';
-    let gameActive = true;
-
-    const winningConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
+   
 
     // Function to create the game board
     function createGameBoard() {
